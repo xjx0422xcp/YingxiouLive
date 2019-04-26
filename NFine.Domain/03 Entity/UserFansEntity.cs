8a64a22e772599d6f515ea81d5b4e9ebed5cdd89
xjx0422xcp <754884317@qq.com>
2019-04-26 13:39:36 +0800

    j

--- NFine.Domain/03 Entity/UserFansEntity.cs
+++ NFine.Domain/03 Entity/UserFansEntity.cs
@@ -18,15 +18,15 @@ namespace NFine.Domain.Entity.UserFans
     /// <summary>
     /// UserFans Entity Model
     /// </summary>
-    public class UserFansEntity : IEntity<UserFansEntity>, ICreationAudited,  IModificationAudited
+    public class UserFansEntity : IEntity<UserFansEntity>, ICreationAudited, IModificationAudited
     {
-						public  String  F_Id { get; set; }
-					public  Int32?  F_UserID { get; set; }
-					public  Int32?  F_ReferID { get; set; }
-					public  Int32?  F_Status { get; set; }
-					public  String  F_CreatorUserId { get; set; }
-					public  DateTime?  F_CreatorTime { get; set; }
-					public  String  F_LastModifyUserId { get; set; }
-					public  DateTime?  F_LastModifyTime { get; set; }
-		    }
+        public String F_Id { get; set; }
+        public Int32? F_UserID { get; set; }
+        public Int32? F_ReferID { get; set; }
+        public Int32? F_Status { get; set; }
+        public String F_CreatorUserId { get; set; }
+        public DateTime? F_CreatorTime { get; set; }
+        public String F_LastModifyUserId { get; set; }
+        public DateTime? F_LastModifyTime { get; set; }
+    }
 }
